33eb171355b103dc3f01df9fbc0684d8b383f892
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-18 12:57:31 +0200

    Populate DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    rating: rand(0..5)
+  )
+end
