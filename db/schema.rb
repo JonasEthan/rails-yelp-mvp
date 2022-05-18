d67584cf6e999432a6000c096940c7db1d0cd65f
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-18 13:54:09 +0200

    Some adjustment to the Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_105543) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_115324) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "phone_number"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -24,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_105543) do
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
